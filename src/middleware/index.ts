0d7e1fc537be6a950eba9518f7d0d72ae3925ee1
Jan Maliński <contact@janmalinski.com>
2023-03-17 15:53:01 +0100

    modify response messages in middleware of handleValidationError and verifyaccessToken

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -14,18 +14,19 @@ class Middleware {
 	async verifyAccessToken(req: any, res: Response, next: NextFunction){
 		const authHeader = req.get('Authorization');
 		if (!authHeader) {
-			return res.status(401).json({error: 'Not authenticated, no authorization header'})
+			return res.status(401).json({message: 'Not authenticated, no authorization header'})
 		}
 		const token = authHeader.split(' ')[1];
+
 		let decodedToken: any;
 		try {
 		  const TOKEN_SECRET: string | undefined = process.env.TOKEN_SECRET;
 		  decodedToken = TOKEN_SECRET && jwt.verify(token, TOKEN_SECRET);
 		} catch (error: any) {
-		  return res.status(500).json({error: `Not authenticated, can't verify access token attached in authorization header`})
+		  return res.status(500).json({message: `Not authenticated, authorization token has expired. Please log in.`})
 		}
 		if (!decodedToken) {
-			return res.status(401).json({error: 'Not authenticated, no access token in authorization header'})
+			return res.status(401).json({message: 'Not authenticated, no access token in authorization header'})
 		}
 		req.userEmail = decodedToken.email;
 		next();
