c34c5a9fe2db7d8626b12849524f4d80495fcb0f
Jan Maliński <contact@janmalinski.com>
2023-03-17 14:14:36 +0100

    add sequalize model relations, switch off https and enable socket.io in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,57 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 // import https from 'https';
 
 import db from "./config/database.config";
-import app from "./app";
-
-import { ServiceModel, UserModel, AdServiceModel, AdModel, TypeemploymentModel, AdTypeemploymentModel, TimeofdayModel, RoleModel, UserRoleModel } from './models';
+import app from './app';
+import { ServiceModel, UserModel, AdServiceModel, AdModel, TypeemploymentModel, AdTypeemploymentModel, TimeofdayModel, RoleModel, UserRoleModel, RoomModel, MessageModel } from './models';
 
 // const key = fs.readFileSync(path.resolve('ssl/key.pem'));
 // const cert = fs.readFileSync(path.resolve('ssl/cert.pem'));
 
-ServiceModel.belongsToMany(AdModel, {through: AdServiceModel, foreignKey: 'service_id'});
-AdModel.belongsToMany(ServiceModel, {through: AdServiceModel, foreignKey: 'ad_id', as: 'Services'});
+RoomModel.hasMany(MessageModel, {foreignKey: 'room_id'});
+MessageModel.belongsTo(RoomModel, { constraints: true,  onDelete: 'CASCADE', as: 'Room'});
 
-AdModel.belongsTo(UserModel, { constraints: true, onDelete: 'CASCADE', as: 'User'});
+RoomModel.belongsTo(AdModel, { constraints: true, onDelete: 'CASCADE', as: 'Ad'});
+AdModel.hasMany(RoomModel, {foreignKey: 'ad_id'});
+
+ServiceModel.belongsToMany(AdModel, {through: {model: AdServiceModel, unique: false}, foreignKey: 'service_id', constraints: false});
+AdModel.belongsToMany(ServiceModel, {through: {model: AdServiceModel, unique: false}, foreignKey: 'ad_id', as: 'Services', constraints: false});
+
+AdModel.belongsTo(UserModel, { constraints: true,  onDelete: 'CASCADE', as: 'User'});
 UserModel.hasMany(AdModel, {foreignKey: 'user_id', as: 'Ads'});
 
-RoleModel.belongsToMany(UserModel, {through: UserRoleModel, foreignKey: 'role_id', as: 'Users'});
-UserModel.belongsToMany(RoleModel, {through: UserRoleModel, foreignKey: 'user_id', as: 'Roles'});
+RoleModel.belongsToMany(UserModel, {through: {model: UserRoleModel, unique: false}, foreignKey: 'role_id', as: 'Users', constraints: false});
+UserModel.belongsToMany(RoleModel, {through: {model: UserRoleModel, unique: false}, foreignKey: 'user_id', as: 'Roles', constraints: false});
 
-TypeemploymentModel.belongsToMany(AdModel, {through: AdTypeemploymentModel, foreignKey: 'typeemployment_id', as: 'Ads'});
-AdModel.belongsToMany(TypeemploymentModel, {through: AdTypeemploymentModel, foreignKey: 'ad_id', as: 'Typeemployments'});
+TypeemploymentModel.belongsToMany(AdModel, {through: {model: AdTypeemploymentModel, unique: false}, foreignKey: 'typeemployment_id', as: 'Ads', constraints: false});
+AdModel.belongsToMany(TypeemploymentModel, {through: {model: AdTypeemploymentModel, unique: false}, foreignKey: 'ad_id', as: 'Typeemployments', constraints: false});
 
 TimeofdayModel.belongsTo(AdModel);
 AdModel.hasOne(TimeofdayModel,  { foreignKey: 'ad_id', as: 'Time'});
 
-// db.sync({force: true}).then(() => {
-db.sync().then(() => {
-	console.log("connect to db");
-});
-
 const port = 9000;
 
-// const server = https.createServer({key, cert }, app);
+// db.sync({force: true}).then(() => {
+db.sync()
+	.then(() => {
+	console.log("connect to db");
+	// const server = https.createServer({key, cert }, app);
+	const server = app.listen(port, () => {
+		console.log("server is running on port " + port);
+	});
+	const io = require('./socket').init(server);
+	io.on('connection', async(socket: any) => {
+		socket.on('join', (data: any) => {	
+			socket.join(data);
+		});
+	})
+})
+.catch((err)=> console.log('ERROR_DB', err));
 
-app.listen(port, () => {
-	console.log("server is running on port " + port);
-});
+
+
+
+
+
 
