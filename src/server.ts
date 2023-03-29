5d2a088684dc9dbe96bcc4568c81db4978dfc4df
Jan Maliński <contact@janmalinski.com>
2023-03-29 10:49:39 +0200

    fix socket.io one to one chat functionality

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@
 // import path from 'path';
 // import https from 'https';
 
+import { Server } from "socket.io";;
+
 import db from "./config/database.config";
 import app from './app';
 import { ServiceModel, UserModel, AdServiceModel, AdModel, TypeemploymentModel, AdTypeemploymentModel, TimeofdayModel, RoleModel, UserRoleModel, RoomModel, MessageModel } from './models';
+
 
 // const key = fs.readFileSync(path.resolve('ssl/key.pem'));
 // const cert = fs.readFileSync(path.resolve('ssl/cert.pem'));
@@ -40,12 +43,47 @@ db.sync()
 	const server = app.listen(port, () => {
 		console.log("server is running on port " + port);
 	});
-	const io = require('./socket').init(server);
-	io.on('connection', async(socket: any) => {
-		socket.on('join', (data: any) => {	
-			socket.join(data);
-		});
-	})
+
+	const io = new Server(server);
+
+	io.on('connection', socket => {
+	  console.log(socket.id + ' ==== connected');
+	
+	  // creating a room name that's unique using both user's unique username
+	
+	  socket.on('join', roomName => {
+	
+	  let split = roomName.split('--with--'); // ['username2', 'username1']
+	
+	  let unique = [...new Set(split)].sort((a: any, b: any) => (a < b ? -1 : 1)); // ['username1', 'username2']
+	
+	  let updatedRoomName = `${unique[0]}--with--${unique[1]}`; // 'username1--with--username2'
+	
+	   Array.from(socket.rooms)
+			.filter(it => it !== socket.id)
+			.forEach(id => {
+			  socket.leave(id);
+			  socket.removeAllListeners(`emitMessage`);
+			});
+	
+	   socket.join(updatedRoomName);
+	
+	   socket.on(`emitMessage`, message => {
+		  Array.from(socket.rooms)
+			   .filter(it => it !== socket.id)
+			   .forEach(id => {
+				  socket.to(id).emit('onMessage', message);
+			   });
+			});
+		  });
+	
+		socket.on('disconnect', () => {
+		  console.log(socket.id + ' ==== diconnected');
+		  socket.removeAllListeners();
+		 });
+	   });
+
+
 })
 .catch((err)=> console.log('ERROR_DB', err));
 
