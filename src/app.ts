61580ac2015a0a8c1b4db0d8ae1042eac7eea410
Jan Maliński <contact@janmalinski.com>
2023-03-29 11:07:11 +0200

    initilaize firebase-admin in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from 'path';
+import admin from 'firebase-admin';
 
 import routes from "./routes";
 import dotenv from 'dotenv';
@@ -9,6 +10,11 @@ const result = dotenv.config();
 if (result.error) {
   throw result.error
 }
+
+const firebaseAccount = require("../firebase.json");
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseAccount),
+});
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
